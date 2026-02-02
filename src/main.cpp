950da06a65ac9c2e2f4da22bc54871c6846a1895
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-02-02 14:13:59 -0500

    getScores function impl and sig change, '#define'so
    - get scores implementation and signature change to support 2D array
      passing.
    
    - changed my global constants into #defines because it makes more sense
      to inline the values for representing array lengths with a define than
      it does to use a const int in the global scope. I don't need to mutate
      the number so there isn't any reason to give it shared state.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,25 @@
+#define NUM_SCORES 5
+#define MAX_STUDENTS 256
+#define UNREACHABLE -1
+
+#include <cstdlib>
+#include <iostream>
 #include <fstream>
 #include <filesystem>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
-
-
-// no nullptr checking
 
 /// returns int number of students. 
 /// Loads the student names into (string *students), and the scores
 /// into 2D-array (int **scores) in an identical order to which student names
 /// are loaded into (string *students).
-int getScores(int **scores, string *students);
+int getScores(int scores[][NUM_SCORES], string *students, filesystem::path fpath);
 
 /// Produces the average for each row of scores within (int **scores), and loads
 /// it into (double *average_scores) in a row-based identical ordering. 
-void averageScores(int **scores, double *average_scores);
-
-// it is faster and more memory efficient to directly format the letter
-// grades into the final outputted string on a line by line basis during iteration
-// of average_scores to produce letter grades than it would be to store another
-// array containing letter grades and have a seperate function which takes both 
-// arrays to produce the formatted report.   
+void averageScores(int scores[][NUM_SCORES], double *average_scores);
 
 /// Assigns each student within (string *students) a letter grade based on their
 /// score from (double *average_scores) and produces a neatly formatted column
@@ -31,20 +29,42 @@ void averageScores(int **scores, double *average_scores);
 ///   - 3. letter grade
 string gradeFormat(double *average_scores, string *students);
 
-
-const char *SCORES_FNAME = "StudentScores.txt";
-
-// A reasonable max for the number of students in a class 
-const int MAX_STUDENTS = 256;
-
-// Number of scores for this particular class
-const int NUM_SCORES = 5;
-
-
 int main(void) {
+  const char *SCORES_FNAME = "StudentScores.txt";
+
   int scores[MAX_STUDENTS][NUM_SCORES];
   double average_scores[MAX_STUDENTS];
-  string students[MAX_STUDENTS];
+  string students[MAX_STUDENTS] {};
+
+  filesystem::path scores_path = 
+    filesystem::current_path().parent_path() / SCORES_FNAME;
+
+  int num_stoods = getScores(scores, students, scores_path); 
 
   return 0;
 }
+
+int getScores(int scores[][NUM_SCORES], string *students, filesystem::path fpath) {
+  const char *ERR_FOPEN = "Error: failed to open file";
+  const char *ERR_FREAD = "Error: file read error";  
+
+  ifstream file { fpath };
+  if (!file) {
+    cerr << ERR_FOPEN << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  int stood_count {};
+  for (; stood_count < MAX_STUDENTS && file >> *students++; ++stood_count) {
+    for (int i {}; i < NUM_SCORES && file >> *scores[i]; ++i);
+  }
+
+  if (file.eof()) {
+    return stood_count;
+  } else if (!file) {
+    cerr << ERR_FREAD << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  return UNREACHABLE;
+} 
